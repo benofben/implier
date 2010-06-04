e9e0dc79a997a54218296147c28671e854537653
benofben <ben.lackey@outlook.com>
2010-06-04 16:42:27 -0700

    Name back to implied.  FIX info gone.  Not sure what this was.

---
+++ Implied/Implied/FormImplied.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using QuickFix;
+
+namespace Implied
+{
+    public partial class FormImplied : Form
+    {
+        public delegate void UpdateConsole();
+        public UpdateConsole consoleDelegate;
+        private SpreadMatrix spreadMatrix;
+
+        FIXApplication fixApplication;
+
+        public FormImplied()
+        {
+            InitializeComponent();
+
+            consoleDelegate = new UpdateConsole(UpdateConsoleMethod);
+            
+            fixApplication = new FIXApplication(this);
+            UpdateConsoleMethod();
+        }
+
+        public void UpdateConsoleMethod()
+        {
+            textBoxConsole.Text = fixApplication.consoleText;
+        }
+
+        private void buttonApply_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // figure out which symbol was requested
+                String[] a = comboBoxSecurity.Text.Split('-');
+                String exchange = a[0];
+                String security = a[1];
+                
+                // request all maturities for the symbol
+                fixApplication.requestSymbols(exchange, security);
+
+                // as the symbols come down, the fix app will create subscribers for each one.
+
+                spreadMatrix = new SpreadMatrix(security);
+            }
+            catch(IndexOutOfRangeException)
+            {
+            }
+
+        }
+
+        private void FormImplied_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+            fixApplication.StopFIX();
+        }
+    }
+}
